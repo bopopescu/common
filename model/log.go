ccd556b6e9c97e34859ec272a7549848fb3ccd9e
lj <lslj0516@163.com>
2019-05-28 20:43:06 +0800

    del: 删除冗余日志接口

--- model/log.go
+++ model/log.go
@@ -3,16 +3,12 @@ package model
 type Logger interface {
 	Info(args ...interface{})
 	Infof(format string, args ...interface{})
-	Infofln(format string, args ...interface{})
 	Debug(args ...interface{})
 	Debugf(format string, args ...interface{})
-	Debugfln(format string, args ...interface{})
 	Warning(args ...interface{})
 	Warningf(format string, args ...interface{})
-	Warningfln(format string, args ...interface{})
 	Error(args ...interface{})
 	Errorf(format string, args ...interface{})
-	Errorfln(format string, args ...interface{})
 	SetLevel(int)
 	GetLevel() int
 }
