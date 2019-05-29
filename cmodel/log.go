a768f8ea41209b00b14aae52ffde665f0673c461
lj <lslj0516@163.com>
2019-05-29 23:18:20 +0800

    mod: optimize model name

--- cmodel/log.go
+++ cmodel/log.go
@@ -1,14 +1,18 @@
-package model
+package cmodel
 
 type Logger interface {
 	Info(args ...interface{})
 	Infof(format string, args ...interface{})
+
 	Debug(args ...interface{})
 	Debugf(format string, args ...interface{})
+
 	Warning(args ...interface{})
 	Warningf(format string, args ...interface{})
+
 	Error(args ...interface{})
 	Errorf(format string, args ...interface{})
+
 	SetLevel(int)
 	GetLevel() int
 }
